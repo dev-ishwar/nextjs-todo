8cb57a5fa07be67db52249c0e68e68f967e70f91
Ishwar Singh <ishwar0647@gmail.com>
2024-12-23 14:01:30 +0530

    Fix UI

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="text-center">
-        <h1 className="text-xl">Welcome to Next-Todo.</h1>
+        <h1 className="text-xl">Welcome Stranger!!</h1>
         <p><Link href={'/login'} className="underline">Login</Link> to proceed.</p>
       </div>
     </div>
