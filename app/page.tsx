fffe3ab02bef7122fc1d36f9f09d3c713f2388b7
Ishwar Singh <ishwar0647@gmail.com>
2024-12-23 13:57:59 +0530

    Update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div>
-        <h1>TODO App</h1>
-        <p></p>
-        <Link href={'/login'}>Sign In</Link>
+      <div className="text-center">
+        <h1 className="text-xl">Welcome to Next-Todo.</h1>
+        <p><Link href={'/login'} className="underline">Login</Link> to proceed.</p>
       </div>
     </div>
   );
